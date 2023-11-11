d0dede8ca2062b189e65b07b5e9c43d18b413ac9
Nikita Thomas <nikitathomas@Nikitas-MBP.lan>
2023-11-11 14:31:54 -0500

    Adding responsiveness for cocktail search

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,25 +3,24 @@ var NameSearchEl = document.querySelector('#namesearch');
 
 document.querySelector('form.cocktailSearchInput').addEventListener('submit', function (e) {
 
-    //prevent the normal submission of the form
     e.preventDefault();
 
     console.log(AlcoholInputEl.value);
     console.log(NameSearchEl.value);
     runCocktailSearch();
-    
 });
 
-var basicAPI = 'https://www.thecocktaildb.com/api/json/v1/1/search.php';
-var searchParam = 'vodka'; 
-var url = `${basicAPI}?s=${searchParam}`;
+// var basicAPI = 'https://www.thecocktaildb.com/api/json/v1/1/search.php';
+// var searchParam = 'vodka'; 
+// var url = '${basicAPI}?s=${searchParam}';
 
-function runCocktailSearch () {
-fetch(url)
-    .then((response) => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    return response.json();
-  })
-}
+// function runCocktailSearch () {
+// fetch(url)
+//     .then((response) => {
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! Status: ${response.status}`);
+//     }
+//     return response.json();
+//   });
+  
+// }
