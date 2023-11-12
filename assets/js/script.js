6c66512f6aa8a1452e5a8b7ab75948345cf17131
Nikita Thomas <nikitathomas@Nikitas-MBP.WiFi Extender>
2023-11-12 16:15:09 -0500

    Correct JS to match previous branch

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,12 +11,20 @@ document.querySelector('form.cocktailSearchInput').addEventListener('submit', fu
 });
 
 function runCocktailSearch () {
-    var searchParam = AlcoholInputEl.value; 
-    var url = 'www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
+    var AlcoholsearchParam = AlcoholInputEl.value; 
+    var AlcoholURL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?i=' + AlcoholsearchParam;
     
-    console.log(url)
-    fetch(url)
+    console.log(AlcoholURL)
+    fetch(AlcoholURL)
         .then(function (response) {
            console.log(response);
-          });
+        });
+    var NamesearchParam = NameSearchEl.value; 
+    var NameURL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + AlcoholInputEl;
+          
+    console.log(NameURL)
+    fetch(NameURL)
+        .then(function (response) {
+            console.log(response);
+        });
     }
